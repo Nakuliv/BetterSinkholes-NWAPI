d27751e48b2cda575a85e4912aa3aec39b8f6ec5
Cwaniak <ccwaniaktv@gmail.com>
2023-04-15 01:34:30 +0200

    Dodaj pliki projektów.

---
+++ BetterSinkoles-NWAPI/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace BetterSinkoles_NWAPI
+{
+    public class Plugin
+    {
+        public static Plugin Instance { get; private set; }
+
+        [PluginAPI.Core.Attributes.PluginEntryPoint("Better Sinkholes", "1.0.0", "", "Nakuliv")]
+        private void Load()
+        {
+            Instance = this;
+
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+            var instance = new Harmony("SinkholePatch");
+            instance.PatchAll();
+        }
+
+        [PluginAPI.Core.Attributes.PluginConfig]
+        public Config Config;
+    }
+}
---
+++ BetterSinkoles-NWAPI/StayingOnSinkholePatch.cs
@@ -0,0 +1,38 @@
+﻿using CustomPlayerEffects;
+using HarmonyLib;
+using Hazards;
+using PlayerRoles;
+using PluginAPI.Core;
+using System;
+using UnityEngine;
+
+namespace BetterSinkoles_NWAPI
+{
+    [HarmonyPatch(typeof(SinkholeEnvironmentalHazard), nameof(SinkholeEnvironmentalHazard.OnEnter))]
+    internal static class StayingOnSinkholePatch
+    {
+        [HarmonyPrefix]
+        private static bool OnEnter(SinkholeEnvironmentalHazard __instance, ReferenceHub player)
+        {
+            try
+            {
+                var ply = Player.Get(player);
+                bool allow = !(ply.Team == Team.SCPs || ply.IsGodModeEnabled);
+
+                if ((double)Vector3.Distance(ply.Position, __instance.transform.position) > (double)__instance.MaxDistance * Plugin.Instance.Config.TeleportDistance)
+                    allow = false;
+
+                ply.EffectsManager.DisableEffect<Sinkhole>();
+                ply.EffectsManager.EnableEffect<Corroding>();
+                ply.SendBroadcast(Plugin.Instance.Config.TeleportMessage, Plugin.Instance.Config.TeleportMessageDuration);
+
+                return allow;
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"StayingOnSinkholePatch failed!\n{ex}");
+                return true;
+            }
+        }
+    }
+}
